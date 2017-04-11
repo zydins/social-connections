f5351211aefd42ee67e1c3323cceb8204b588ca2
zydins <s.zudin@webeffector.ru>
2017-04-11 17:09:24 +0300

    prepare output to the second stage

--- src/main/java/ru/zudin/social/Executor.java
+++ src/main/java/ru/zudin/social/Executor.java
@@ -2,7 +2,9 @@ package ru.zudin.social;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.util.ToolRunner;
 import ru.zudin.ChainingJob;
 import ru.zudin.social.mr.HashMapper;
@@ -20,51 +22,40 @@ public class Executor {
 
     public static void main(String[] args) throws Exception {
         FileSystem fileSystem = FileSystem.get(new Configuration());
-        fileSystem.delete(new Path("output"), true);
-        fileSystem.delete(new Path("temp1"), true);
+        profileMatching(fileSystem);
+
+
+    }
+
+    private static void profileMatching(FileSystem fileSystem) throws Exception {
+        Path outputPath = new Path("output");
+        fileSystem.delete(outputPath, true);
+        Path tempPath = new Path("temp");
+        fileSystem.delete(tempPath, true);
 
         ChainingJob job = ChainingJob.Builder.instance()
                 .name("social_connections")
-                .tempDir("temp")
+                .tempDir(tempPath.getName())
 //                .mapper(EntityMapper.class)
                 .mapper(HashMapper.class)
-//                .reducer(UserMatchReducer.class, Collections.singletonMap("-M", "true"))
                 .reducer(UserMatchReducer.class)
                 .mapper(UserPairMapper.class)
                 .reducer(UserCollectReducer.class, Collections.singletonMap("-M", "true"))
                 .build();
 
         ToolRunner.run(new Configuration(), job, new String[]{"input/users.txt", "output"});
-
-
-
-//        Preconditions.checkArgument(args.length >= 1);
-//        Configuration configuration = new Configuration();
-//        Job job = Job.getInstance(configuration, "alertbutton");
-//        job.setJarByClass(Executor.class);
-//        Class<DeviceMapper> mapperClass = DeviceMapper.class;
-//        Class<GeoReducer> reducerClass = GeoReducer.class;
-//        job.setMapperClass(mapperClass);
-//        Class<?>[] typeArgs = TypeResolver.resolveRawArguments(Mapper.class, mapperClass);
-//        job.setMapOutputKeyClass(typeArgs[2]);
-//        job.setMapOutputValueClass(typeArgs[3]);
-//        typeArgs = TypeResolver.resolveRawArguments(Reducer.class, reducerClass);
-//        job.setOutputKeyClass(typeArgs[2]);
-//        job.setOutputValueClass(typeArgs[3]);
-//        job.setReducerClass(reducerClass);
-//
-//        FileSystem fileSystem = FileSystem.get(new Configuration());
-//        Path input = new Path(args[0]);
-//        Path output = new Path("output");
-//        fileSystem.delete(output, true);
-//        FileInputFormat.addInputPath(job, input);
-//        FileOutputFormat.setOutputPath(job, output);
-//
-//        if (!job.waitForCompletion(true)) {
-//            System.out.println("Cannot complete job");
-//        }
-//        System.out.println("Done");
-
+        RemoteIterator<LocatedFileStatus> iterator = fileSystem.listFiles(outputPath, true);
+        while (iterator.hasNext()) {
+            LocatedFileStatus next = iterator.next();
+            Path path = next.getPath();
+            String name = path.getName();
+            if (!name.endsWith("crc") && !name.equals("_SUCCESS")) {
+                String newName = name.replace("-r-00000", "");
+                fileSystem.rename(path, new Path("match/" + newName));
+            }
+        }
+        fileSystem.delete(outputPath, true);
+        fileSystem.delete(tempPath, true);
     }
 
 }
